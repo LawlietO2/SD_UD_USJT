c5abd4ff3475d1300bc011e1717d22b3a58824cb
amaoliv <amand2oliv@gmail.com>
2022-10-24 22:38:14 -0300

    arrumando repositorio

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+
+export interface pacientes {
+  nome: string;
+  data: Date; //Mudar para Date
+  prioridade: string;
+}
+
+// teste
+
+const ELEMENT_DATA: pacientes[] = [
+
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'interface';
+
+  constructor(private dialog : MatDialog){
+
+  }
+
+  displayedColumns: string[] = ['prioridade', 'nome'];
+  dataSource = ELEMENT_DATA;
+  
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%'
+    });
+  }
+}
---
+++ frontend/src/app/dialog/dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  listaPrioridade = ["Não Urgente", "Pouco Urgente", "Urgente", "Emergência"];
+  pacienteForm !: FormGroup;  
+  constructor(private formBuilder : FormBuilder, private api : ApiService, private dialogRef : MatDialogRef<DialogComponent>){
+
+  }
+
+  ngOnInit(): void {
+    this.pacienteForm = this.formBuilder.group({
+      nome : ['',Validators.required],
+      prioridade : ['',Validators.required]
+    })
+  }
+  addPaciente(){
+    if(this.pacienteForm.valid){
+      this.api.postPaciente(this.pacienteForm.value)    
+      .subscribe({
+        next:(res)=>{
+          alert("Paciente adicionado");
+          this.pacienteForm.reset();
+          this.dialogRef.close();
+        },
+        error:()=>{
+          alert("Erro")
+        }
+      })
+    }
+  }
+
+}
