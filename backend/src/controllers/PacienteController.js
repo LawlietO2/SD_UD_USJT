f7d29484e3027e8e77669eac5cd6beb668fc1c66
amaoliv <amand2oliv@gmail.com>
2022-10-24 22:39:23 -0300

    arrumando repositorio

---
+++ backend/src/controllers/PacienteController.js
@@ -0,0 +1,89 @@
+const PacienteService = require('../services/PacienteService');
+
+module.exports = {
+    buscarTodos: async (req, res)=>{
+        let json = {error:'', result:[]};
+
+        let pacientes = await PacienteService.buscarTodos();
+
+        for(let i in pacientes){
+            json.result.push({
+                id: pacientes[i].id,
+                nome: pacientes[i].nome,
+                estado: pacientes[i].status,
+                prioridade: pacientes[i].prioridade,
+                data: pacientes[i].datalocal
+            });
+        }
+    res.json(json);
+    },
+    buscarUm: async (req, res)=>{
+        let json = {error:'', result:{}};
+
+        let id = req.params.id;
+        let paciente = await PacienteService.buscarUm(id);
+        
+        if(paciente){
+            json.result = paciente;
+        }
+        console.log(json);
+        res.json(json);
+    },
+    inserir: async (req, res)=>{
+        let json = {error:'', result:{}};
+
+        let nome = req.body.nome;
+        let estado = "Pendente";
+        let prioridade = req.body.prioridade;
+        let data = new Date();
+
+        if(nome && estado && prioridade && data){
+            let pacienteId = await PacienteService.inserir(nome, estado, prioridade, data);
+            json.result = {
+                id: pacienteId,
+                nome,
+                estado,
+                prioridade,
+                data
+            };
+        }else{
+            json.error = 'Campos não enviados'
+        }
+
+        res.json(json);
+    },
+    alterar: async (req, res)=>{
+        let json = {error:'', result:{}};
+
+        let id = req.params.id;
+        let nome = req.body.nome;
+        let estado = req.body.estado;
+        let prioridade = req.body.prioridade;
+        let date = new Date();
+
+        if(id && nome && estado && prioridade && data){
+            await PacienteService.alterar(id, nome, estado, prioridade, data);
+            json.result = {
+                id,
+                nome,
+                estado,
+                prioridade,
+                data
+            };
+        }else{
+            json.error = 'Campos não enviados'
+        }
+
+        res.json(json);
+    },
+    excluir: async (req, res)=>{
+
+        let json = {error:'', result:{}};
+
+        let paciente = await PacienteService.excluir(req.params.id);
+
+        res.json(json);
+    }
+}
+
+// teste
