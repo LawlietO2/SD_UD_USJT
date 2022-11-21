393112a0c1d286fa802a3a2edd3b9e8aea458a95
amaoliv <amand2oliv@gmail.com>
2022-11-20 22:03:34 -0300

    adicionando info no readme e os scripts do db

---
+++ database_script/insert_pacientes.sql
@@ -0,0 +1,3 @@
+USE pacientes_db;
+
+INSERT INTO pacientes(nome,status,prioridade,datalocal,consulta_cod,especialidade) VALUES ( 'Frederico Kaneto', 'Pendente','Pouco Urgente','2022-11-18 22:28:31','86469080','Clinico Geral');
